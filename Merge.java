e71de11bcd5710518001ec5d1890404e9b918cb8
Tammy Chen <tchen00@stuy.edu>
2019-03-15 11:39:06 -0400

    find split

--- Merge.java
+++ Merge.java
@@ -1,12 +1,34 @@
 public class Merge{
   /*sort the array from least to greatest value. This is a wrapper function*/
+  /*
+    PSEUDOCODE from website: 
+      mergesort(data,lo,hi):
+        if lo >= hi :
+          return
+        mergesort left side
+        mergesort right side
+        merge
+ */
   public static void mergesort(int[]data){
+    return;
+  }
 
+  private static void mergesort(int[]data, int[]temp, int lo, int hi){
+    if (lo >= hi) return;
+    //mergesort()
+  }
 
+  public static int findSplit(int[] data){
+    for (int i = 0; i < data.length; i++){
+      if (data[i] > data[i+1]){
+        return i;
+      }
+    }
+    return data.length - 1;
   }
 
   public static void main(String[] args){
-    
+
   }
 
 }
