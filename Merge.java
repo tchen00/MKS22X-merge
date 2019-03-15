e74fe1170b2cbd1b7a18a653aef1c3428d6ac7b4
Tammy Chen <tchen00@stuy.edu>
2019-03-15 11:27:43 -0400

    add method skeleton

--- Merge.java
+++ Merge.java
@@ -1,4 +1,12 @@
 public class Merge{
+  /*sort the array from least to greatest value. This is a wrapper function*/
+  public static void mergesort(int[]data){
 
-  
+
+  }
+
+  public static void main(String[] args){
+    
+  }
+
 }
