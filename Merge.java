3e0f7842fa56a2f703a1154d4979086b8b9f6f55
Tammy <tchen00@stuy.edu>
2019-03-18 11:27:38 -0400

    add merge Helper :

--- Merge.java
+++ Merge.java
@@ -16,15 +16,41 @@ public class Merge{
   private static void mergesort(int[]data, int[]temp, int lo, int hi){
     if (lo >= hi) return;
     mergesort(data, temp, low, (hi + low) / 2);
-    mergesort(data, temp, low, (hi + low) / 2);
+    mergesort(data, temp, (hi + low) / 2, hi);
 
     //mergesort()
   }
+
+  private static void mergeH(int[] data){
+    int middle = data.length / 2;
+    int[] left = new int[middle];
+    int[] right = new int[data.length - middle];
+    // looping through the left side
+    for (int i = 0; i < middle; i++){
+      left[i] = data[i];
+    }
+    // looping through the right side
+    for (int j = middle; j < data.length; j++){
+      second[j - middle] = data[j];
+    }
+
+    if (left.length > 1) {
+      mergeH(left);
+    }
+    if (right.length > 1) {
+      mergeH(right);
+    }
+
+    merge(data, left, right);
+  }
+
+
 
   public static int[] subArr(int[] data, int start, int end){
     int[] ary = new int[end-start];
     return ary;
   }
+
 /*
   public static int findSplit(int[] data){
     for (int i = 0; i < data.length; i++){
