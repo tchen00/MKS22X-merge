7db78235517a97fb66722a82531220e867dc7f21
Tammy <tchen00@stuy.edu>
2019-03-19 11:43:18 -0400

    add drivers and test code

--- Merge.java
+++ Merge.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.io.*;
+
 public class Merge{
   /*sort the array from least to greatest value. This is a wrapper function*/
   /*
@@ -59,7 +62,7 @@ public class Merge{
   }
 */
 
-  public static void merge(int[] data, int[] left, int[] rigth) {
+  public static void merge(int[] data, int[] left, int[] right) {
     // variables for left, right and current data index
     int i = 0;
     int leftI = 0;
@@ -99,7 +102,7 @@ public class Merge{
     int[] ary = new int[end-start];
     return ary;
   }
-  
+
   public static int findSplit(int[] data){
     for (int i = 0; i < data.length; i++){
       if (data[i] > data[i+1]){
@@ -109,11 +112,39 @@ public class Merge{
     return data.length - 1;
   }
 */
-  public static void main(String[] args){
-    /*
-    int[] data = {2,3,4,1,2,3};
-    System.out.println(findSplit(data));
-    */
+public static void main(String[]args){
+  //USING MR. K'S sort from last time 
+  System.out.println("Size\t\tMax Value\tmerge/builtin ratio ");
+  int[]MAX_LIST = {1000000000,500,10};
+  for(int MAX : MAX_LIST){
+    for(int size = 31250; size < 2000001; size*=2){
+      long qtime=0;
+      long btime=0;
+      for(int trial = 0 ; trial <=5; trial++){
+        int []data1 = new int[size];
+        int []data2 = new int[size];
+        for(int i = 0; i < data1.length; i++){
+          data1[i] = (int)(Math.random()*MAX);
+          data2[i] = data1[i];
+        }
+        long t1,t2;
+        t1 = System.currentTimeMillis();
+        Merge.mergesort(data2);
+        t2 = System.currentTimeMillis();
+        qtime += t2 - t1;
+        t1 = System.currentTimeMillis();
+        Arrays.sort(data1);
+        t2 = System.currentTimeMillis();
+        btime+= t2 - t1;
+        if(!Arrays.equals(data1,data2)){
+          System.out.println("FAIL TO SORT!");
+          System.exit(0);
+        }
+      }
+      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+    }
+    System.out.println();
   }
+}
 
 }
